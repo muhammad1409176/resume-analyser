c283c948790c813d0377dd89db721073c4d5827f
muhammad1409176 <mohammedmusaib651@gmail.com>
2026-03-27 21:42:50 +0530

    fix render port

--- backend/app.py
+++ backend/app.py
@@ -174,4 +174,9 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    app = create_app()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
